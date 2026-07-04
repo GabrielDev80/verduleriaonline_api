6020fa3f1b7adaa33db15989f3b62156d259f29c
Gabriel Nicolosi <gabianp@live.com.ar>
2026-07-04 13:36:03 -0300

    Create(Constants) - Crea constantes para Orders.

---
+++ src/constants/order.constants.js
@@ -0,0 +1,13 @@
+export const ORDER_STATUS = [
+  "pending",
+  "confirmed",
+  "preparing",
+  "ready",
+  "delivering",
+  "completed",
+  "cancelled",
+];
+
+export const DELIVERY_METHODS = ["pickup", "delivery"];
+
+export const PAYMENT_METHODS = ["cash", "mercadopago", "transfer-bank"];
